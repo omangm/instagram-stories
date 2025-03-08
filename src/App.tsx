9cb3468ff09c685b2d85b1b34820fa63b52bce0e
omang <omangmorekar68@gmail.com>
2025-03-08 07:40:16 +0530

    add a 404 page that redirects to the home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { JSX } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StoriesHome from './components/StoriesList';
 import StoryViewer from './components/StoryViewer';
+import NotFound from './components/NotFound';
 
 function App(): JSX.Element {
   return (
@@ -9,6 +10,7 @@ function App(): JSX.Element {
       <Routes>
         <Route path="/" element={<StoriesHome />} />
         <Route path="/stories/:userId/:storyId" element={<StoryViewer />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
   );
